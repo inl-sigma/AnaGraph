79f20f4ea44c3aa41cef2bd2945ae366f6385049
inl-sigma <sigma@inl.ics.keio.ac.jp>
2024-08-19 11:02:08 +0900

    feat: Enable testing feature for WeightedSuperDigraph

--- test/weighted_superdigraph_test.cpp
+++ test/weighted_superdigraph_test.cpp
@@ -1,17 +1,20 @@
 #include "supergraph.hpp"
 
 #include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
 
 TEST(WeightedSuperDigraphTest, SetNode) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
     graph.setNode(4);
 
-    EXPECT_EQ(graph.size(), 3);
+    EXPECT_EQ(graph.size(), static_cast<size_t>(3));
 }
 
 TEST(WeightedSuperDigraphTest, GetNode) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -27,6 +30,7 @@ TEST(WeightedSuperDigraphTest, GetNode) {
 }
 
 TEST(WeightedSuperDigraphTest, RemoveNode) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -34,14 +38,36 @@ TEST(WeightedSuperDigraphTest, RemoveNode) {
 
     graph.removeNode(2);
 
-    EXPECT_EQ(graph.size(), 2);
-    EXPECT_THROW(graph.getNode(2), std::out_of_range);
+    EXPECT_EQ(graph.size(), static_cast<size_t>(2));
 
     WeightedSupernode node1 = graph.getNode(1);
     EXPECT_EQ(node1.getId(), 1);
 }
 
+TEST(WeightedSuperDigraphTest, GetIds) {
+    spdlog::set_level(spdlog::level::debug);
+    WeightedSuperDigraph graph;
+    graph.setNode(1);
+    graph.setNode(3);
+    graph.setNode(4);
+
+    std::unordered_set<int> ids = graph.getIds();
+
+    EXPECT_EQ(ids.size(), static_cast<size_t>(3));
+    EXPECT_TRUE(ids.contains(1));
+    EXPECT_TRUE(ids.contains(3));
+    EXPECT_TRUE(ids.contains(4));
+
+    graph.removeNode(3);
+    ids = graph.getIds();
+
+    EXPECT_EQ(ids.size(), static_cast<size_t>(2));
+    EXPECT_TRUE(ids.contains(1));
+    EXPECT_TRUE(ids.contains(4));
+}
+
 TEST(WeightedSuperDigraphTest, AddEdge) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -55,6 +81,7 @@ TEST(WeightedSuperDigraphTest, AddEdge) {
 }
 
 TEST(WeightedSuperDigraphTest, RemoveEdge) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -70,6 +97,7 @@ TEST(WeightedSuperDigraphTest, RemoveEdge) {
 }
 
 TEST(WeightedSuperDigraphTest, GetWeight) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -83,6 +111,7 @@ TEST(WeightedSuperDigraphTest, GetWeight) {
 }
 
 TEST(WeightedSuperDigraphTest, SetWeight) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -99,6 +128,7 @@ TEST(WeightedSuperDigraphTest, SetWeight) {
 }
 
 TEST(WeightedSuperDigraphTest, AddWeight) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -115,6 +145,7 @@ TEST(WeightedSuperDigraphTest, AddWeight) {
 }
 
 TEST(WeightedSuperDigraphTest, GetAndSetParent) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -130,6 +161,7 @@ TEST(WeightedSuperDigraphTest, GetAndSetParent) {
 }
 
 TEST(WeightedSuperDigraphTest, UpdateParent) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -144,6 +176,7 @@ TEST(WeightedSuperDigraphTest, UpdateParent) {
 }
 
 TEST(WeightedSuperDigraphTest, RemoveParent) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -158,6 +191,7 @@ TEST(WeightedSuperDigraphTest, RemoveParent) {
 }
 
 TEST(WeightedSuperDigraphTest, GetChildren) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -171,14 +205,15 @@ TEST(WeightedSuperDigraphTest, GetChildren) {
     std::unordered_set<int> children1 = graph.getChildren(1);
     std::unordered_set<int> children2 = graph.getChildren(2);
 
-    EXPECT_EQ(children1.size(), 1);
-    EXPECT_EQ(children2.size(), 2);
+    EXPECT_EQ(children1.size(), static_cast<size_t>(1));
+    EXPECT_EQ(children2.size(), static_cast<size_t>(2));
     EXPECT_TRUE(children1.contains(2));
     EXPECT_TRUE(children2.contains(3));
     EXPECT_TRUE(children2.contains(4));
 }
 
 TEST(WeightedSuperDigraphTest, GetAdjacents) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
@@ -190,31 +225,34 @@ TEST(WeightedSuperDigraphTest, GetAdjacents) {
     graph.addEdge(1, 2, 5.0);
 
     graph.setParent(3, 4);
+    graph.addEdge(3, 2, 1.0);
+    graph.addEdge(3, 5, -3.0);
     graph.addEdge(4, 5, 3.0);
     graph.addEdge(4, 6, 2.0);
-    graph.addEdge(3, 4, -3.0);
 
     const std::unordered_map<int, double>& adjacents1 = graph.getAdjacents(1);
-    EXPECT_EQ(adjacents1.size(), 1);
-    EXPECT_EQ(adjacents1.at(2), 5.0);
+    EXPECT_EQ(adjacents1.size(), static_cast<size_t>(1));
+    EXPECT_DOUBLE_EQ(adjacents1.at(2), 5.0);
 
     const std::unordered_map<int, double>& adjacents3 = graph.getAdjacents(3);
-    EXPECT_EQ(adjacents3.size(), 1);
-    EXPECT_FALSE(adjacents3.contains(4));
-    EXPECT_EQ(graph.getWeight(3, 4), -3.0);
-    EXPECT_EQ(adjacents3.at(6), 2.0);
+    EXPECT_EQ(adjacents3.size(), static_cast<size_t>(2));
+    EXPECT_DOUBLE_EQ(adjacents3.at(2), 1.0);
+    EXPECT_DOUBLE_EQ(adjacents3.at(6), 2.0);
+    EXPECT_DOUBLE_EQ(graph.getWeight(3, 5), -3.0);
 }
 
 TEST(WeightedSuperDigraphTest, Size) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
     graph.setNode(3);
 
-    EXPECT_EQ(graph.size(), 3);
+    EXPECT_EQ(graph.size(), static_cast<size_t>(3));
 }
 
 TEST(WeightedSuperDigraphTest, ReadGraph) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.readGraph("../../dataset/input", FileExtension::TXT);
 
@@ -235,12 +273,23 @@ TEST(WeightedSuperDigraphTest, ReadGraph) {
 }
 
 TEST(WeightedSuperDigraphTest, WriteGraph) {
+    spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
     graph.setNode(2);
     graph.setNode(3);
-    graph.addEdge(1, 2, 5.0);
-    graph.addEdge(2, 3, 3.0);
+    graph.setNode(4);
+    graph.setNode(5);
+    graph.setNode(6);
+
+    graph.setParent(1, 5);
+    graph.setParent(2, 5);
+    graph.setParent(3, 6);
+    graph.setParent(4, 6);
+
+    graph.addEdge(1, 2, 1.0);
+    graph.addEdge(3, 4, 3.0);
+    graph.addEdge(5, 6, 2.0);
 
     graph.writeGraph("../../dataset/output", FileExtension::TXT);
 }
