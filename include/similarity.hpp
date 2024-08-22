a5411bb104c46f4e23bc04d1b1d72eb656b25e30
inl-sigma <sigma@inl.ics.keio.ac.jp>
2024-08-22 17:57:32 +0900

    feat: Add nDCG calculation to similarity module

--- include/similarity.hpp
+++ include/similarity.hpp
@@ -15,6 +15,33 @@ namespace similarity {
      */
     double cosineSimilarity(const std::vector<double>& v1, const std::vector<double>& v2);
 
+    /**
+     * Calculates the normalized Discounted Cumulative Gain (nDCG) for a given set of expected and answer values.
+     * 
+     * The nDCG is calculated by the difference of ranking between the expected and answer values.
+     *
+     * @param expected The vector of expected values.
+     * @param answer The vector of answer values.
+     * @param k The number of elements to consider in the calculation.
+     * @return The nDCG value.
+     * 
+     * @note The vectors must have the same size. The value of k must be less than or equal to the size of the vectors.
+     */
+    double nDCG(const std::vector<double>& expected, const std::vector<double>& answer, int k);
+
+    /**
+     * Calculates the normalized Discounted Cumulative Gain (nDCG) for a given set of expected and answer values.
+     * 
+     * The nDCG is calculated by the difference of ranking between the expected and answer values.
+     *
+     * @param expected The vector of expected values.
+     * @param answer The vector of answer values.
+     * @return The nDCG value.
+     * 
+     * @note The vectors must have the same size.
+     */
+    double nDCG(const std::vector<double>& expected, const std::vector<double>& answer);
+
 } // namespace similarity
 
 #endif // SIMILARITY_HPP
--- src/similarity.cpp
+++ src/similarity.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <stdexcept>
+#include <unordered_map>
 
 namespace similarity {
 
@@ -21,5 +22,50 @@ double cosineSimilarity(const std::vector<double>& v1, const std::vector<double>
     }
     return dotProduct / (std::sqrt(normA) * std::sqrt(normB));
 }
+
+double nDCG(const std::vector<double>& expected, const std::vector<double>& answer, int k) {
+    const size_t v1Size = expected.size();
+    const size_t v2Size = answer.size();
+    if (v1Size != v2Size) {
+        throw std::invalid_argument("Vectors must have the same size");
+    }
+    if (static_cast<size_t>(k) > v1Size) {
+        throw std::invalid_argument("k must be less than or equal to the size of the vectors");
+    }
+    if (k <= 0) {
+        throw std::invalid_argument("k must be greater than 0");
+    }
+
+    std::vector<std::pair<double, int>> expectedMap;
+    std::vector<std::pair<double, int>> answerMap;
+    for (size_t i = 0; i < expected.size(); i++) {
+        expectedMap.emplace_back(std::make_pair(expected[i], i));
+        answerMap.emplace_back(std::make_pair(answer[i], i));
+    }
+    std::sort(expectedMap.begin(), expectedMap.end(), [](const std::pair<double, int>& a, const std::pair<double, int>& b) {
+        return a.first > b.first;
+    });
+    std::sort(answerMap.begin(), answerMap.end(), [](const std::pair<double, int>& a, const std::pair<double, int>& b) {
+        return a.first > b.first;
+    });
+    
+    double dcg = 0.0;
+    double idcg = 0.0;
+    for (int i = 0; i < k; i++) {
+        int expectedIdx = expectedMap[i].second;
+        int answerIdx = answerMap[i].second;
+
+        if (expectedIdx == answerIdx) {
+            dcg += (1 / std::log2(i + 2));
+        }
+        idcg += (1 / std::log2(i + 2));
+    }
+    return dcg / idcg;
+}
+
+double nDCG(const std::vector<double>& expected, const std::vector<double>& answer) {
+    return nDCG(expected, answer, expected.size());
+}
+
 
 } // namespace similarity
