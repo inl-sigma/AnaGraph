57306921605e28856e2d46ec017721c552d09ecc
inl-sigma <sigma@inl.ics.keio.ac.jp>
2024-08-22 12:07:46 +0900

    feat: Implement HeteroGraph class and methods for graph manipulation (close #16)

--- include/heterogeneous_graph.hpp
+++ include/heterogeneous_graph.hpp
@@ -99,11 +99,11 @@ public:
 };
 
 /**
- * @class UnweightedHeteroDigraph
+ * @class HeteroDigraph
  * @brief Represents a weighted graph data structure.
  * 
- * @note This UnweightedHeteroDigraph class is indexed as 0-origin and allowed to have self-loops.
- * The UnweightedHeteroDigraph class provides a way to represent a weighted graph using an adjacency list.
+ * @note This HeteroDigraph class is indexed as 0-origin and allowed to have self-loops.
+ * The HeteroDigraph class provides a way to represent a graph using an adjacency list.
  * It supports adding edges between vertices and accessing the adjacency list of a node.
  */
 template <typename T>
@@ -265,6 +265,160 @@ private:
      * It uses the specified writer to write the graph.
      */
     void writeGraphHelper(std::string filename, IGraphWriter &writer, std::vector<EdgeObject> &edges) const;
+};
+
+/**
+ * @class HeteroGraph
+ * @brief Represents a graph data structure.
+ * 
+ * @note This HeteroGraph class is indexed as 0-origin and allowed to have self-loops.
+ * The HeteroGraph class provides a way to represent a graph using an adjacency list.
+ * It supports adding edges between vertices and accessing the adjacency list of a node.
+ */
+template <typename T>
+class HeteroGraph : public IHeteroDigraph<T> {
+private:
+    HeteroDigraph<T> digraph;
+
+public:
+    /**
+     * @brief Constructs a HeteroGraph object.
+     */
+    HeteroGraph() = default;
+
+    /**
+     * @brief Constructs a HeteroGraph object with the file.
+     * @param filePath The path to the file containing the graph data.
+     */
+    HeteroGraph(std::string filePath, FileExtension extName);
+
+    /**
+     * @brief Copy constructor for the HeteroGraph object.
+     */
+    HeteroGraph(const HeteroGraph<T> &graph) 
+        : digraph(graph.digraph) {
+    }
+
+    /**
+     * @brief Assignment operator for the HeteroGraph object.
+     */
+    HeteroGraph<T>& operator=(const HeteroGraph<T>& graph) = default;
+
+    /** 
+     * @brief Get the attributes of a node.
+     * @param id The node to get the attributes of
+     * @return A copy of the attributes of the node
+     * 
+     * @todo override interface method after implementing the method
+     * 
+     * This method is used to access the attributes of a node.
+     */
+    HeteroNode<T> getNode(int id) const;
+
+    /**
+     * @brief Set a node to the graph.
+     * @param id The node to add
+     */
+    void setNode(int id) override;
+
+    /**
+     * @brief Set a node to the graph.
+     * @param node The node to add
+     * 
+     * @todo override interface method after implementing the method
+     */
+    void setNode(HeteroNode<T> &node);
+
+    /**
+     * @brief Remove a node from the graph.
+     * @param id The node to remove
+     */
+    void removeNode(int id) override;
+
+    /**
+     * @brief Get the id of the graph.
+     */
+    std::unordered_set<int> getIds() const override;
+
+    /**
+     * @brief Add an edge between two nodes.
+     * @param src The source node
+     * @param dst The destination node
+     */
+    void addEdge(int src, int dst) override;
+
+    /**
+     * @brief Remove an edge between two nodes.
+     * @param src The source node
+     * @param dst The destination node
+     */
+    void removeEdge(int src, int dst) override;
+
+    /**
+     * @brief Get the adjacent nodes of a node.
+     * @param id The source node
+     * @return A set of integers representing the adjacent nodes.
+     */
+    const std::unordered_set<int> getAdjacents(int id) const override;
+
+    /**
+     * @brief Get the subgraph of the graph.
+     * @param indices The indices of the nodes to include in the subgraph
+     * @return A subgraph of the graph
+     */
+    HeteroGraph<T> getSubgraph(std::unordered_set<int> indices) const;
+
+    /**
+     * @brief Reorganize the graph.
+     * 
+     * This method is used to reorganize the graph.
+     * It can be used to delete unnecessary nodes.
+     */
+    void reorganize();
+
+    /**
+     * @brief Get the attributes of the nodes. 
+     * @param id The id of the node to get the attributes of
+     * @return A vector of the attributes of the nodes
+     */
+    T getAttributes(int id) const override;
+
+    /**
+     * @brief Set the attributes of the nodes.
+     * @param id The id of the node to set the attributes of
+     * @param attribute The attributes of the nodes
+     */
+    void setAttributes(int id, T attributes) override;
+
+    /**
+     * Converts the heterogeneous graph to a directed graph.
+     *
+     * @return The converted directed graph.
+     */
+    HeteroDigraph<T> toDigraph() const;
+
+    /**
+     * @brief Get the number of nodes in the graph.
+     */
+    size_t size() const override;
+
+    /**
+     * @brief Read a graph from a file.
+     * @param filename The name of the file to import the graph from
+     * @param extName The extension of the file
+     * 
+     * @todo read graph as a heterogeneous graph, not a weighted graph
+     */
+    void readGraph(std::string filename, FileExtension extName) override;
+
+    /**
+     * @brief Write the graph to a file.
+     * @param filename The name of the file to export the graph to
+     * @param extName The extension of the file
+     * 
+     * @todo write graph as a heterogeneous graph, not a weighted graph
+     */
+    void writeGraph(std::string filename, FileExtension extName) const override;
 };
 
 /**
--- src/heterogeneous_graph.cpp
+++ src/heterogeneous_graph.cpp
@@ -299,6 +299,109 @@ void HeteroDigraph<T>::writeGraphHelper(std::string filename, IGraphWriter &writ
     writer.writeGraph(filename, edges);
 }
 
+template <typename T>
+HeteroGraph<T>::HeteroGraph(std::string filePath, FileExtension extName) {
+    digraph.readGraph(filePath, extName);
+}
+
+template <typename T>
+HeteroNode<T> HeteroGraph<T>::getNode(int id) const {
+    return digraph.getNode(id);
+}
+
+template <typename T>
+void HeteroGraph<T>::setNode(int id) {
+    digraph.setNode(id);
+}
+
+template <typename T>
+void HeteroGraph<T>::setNode(HeteroNode<T> &node) {
+    digraph.setNode(node);
+}
+
+template <typename T>
+void HeteroGraph<T>::removeNode(int id) {
+    digraph.removeNode(id);
+}
+
+template <typename T>
+std::unordered_set<int> HeteroGraph<T>::getIds() const {
+    return digraph.getIds();
+}
+
+template <typename T>
+void HeteroGraph<T>::addEdge(int src, int dst) {
+    digraph.addEdge(src, dst);
+    digraph.addEdge(dst, src);
+}
+
+template <typename T>
+void HeteroGraph<T>::removeEdge(int src, int dst) {
+    digraph.removeEdge(src, dst);
+    digraph.removeEdge(dst, src);
+}
+
+template <typename T>
+const std::unordered_set<int> HeteroGraph<T>::getAdjacents(int id) const {
+    return digraph.getAdjacents(id);
+}
+
+template <typename T>
+HeteroGraph<T> HeteroGraph<T>::getSubgraph(std::unordered_set<int> indices) const {
+    HeteroGraph<T> subgraph;
+    subgraph.digraph = digraph.getSubgraph(indices);
+    return subgraph;
+}
+
+template <typename T>
+void HeteroGraph<T>::reorganize() {
+    digraph.reorganize();
+}
+
+template <typename T>
+T HeteroGraph<T>::getAttributes(int id) const {
+    return digraph.getAttributes(id);
+}
+
+template <typename T>
+void HeteroGraph<T>::setAttributes(int id, T attributes) {
+    digraph.setAttributes(id, attributes);
+}
+
+template <typename T>
+HeteroDigraph<T> HeteroGraph<T>::toDigraph() const {
+    return digraph;
+}
+
+template <typename T> 
+size_t HeteroGraph<T>::size() const {
+    return digraph.size();
+}
+
+template <typename T>
+void HeteroGraph<T>::readGraph(std::string filename, FileExtension extName) {
+    digraph.readGraph(filename, extName);
+    HeteroDigraph<T> deepCopy = digraph;
+    for (int id : digraph.getIds()) {
+        for (int adj : deepCopy.getAdjacents(id)) {
+            digraph.addEdge(adj, id);
+        }
+    }
+}
+
+template <typename T>
+void HeteroGraph<T>::writeGraph(std::string filename, FileExtension extName) const {
+    HeteroDigraph<T> digraph = toDigraph();
+    for (int id : digraph.getIds()) {
+        for (int adj : digraph.getAdjacents(id)) {
+            if (id > adj) {
+                digraph.removeEdge(id, adj);
+            }
+        }
+    }
+    digraph.writeGraph(filename, extName);
+}
+
 
 template <typename T>
 WeightedHeteroNode<T>& WeightedHeteroNode<T>::operator=(const WeightedHeteroNode<T>& node) {
@@ -788,6 +891,10 @@ template class HeteroNode<std::any>;
 template class HeteroDigraph<int>;
 template class HeteroDigraph<std::string>;
 template class HeteroDigraph<std::any>;
+
+template class HeteroGraph<int>;
+template class HeteroGraph<std::string>;
+template class HeteroGraph<std::any>;
 
 template class WeightedHeteroNode<int>;
 template class WeightedHeteroNode<std::string>;
