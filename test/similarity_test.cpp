c64ca4530290558bafbfe2c46513dedac3967eb5
inl-sigma <sigma@inl.ics.keio.ac.jp>
2024-08-22 17:57:26 +0900

    test: Add nDCG test for similarity module

--- test/similarity_test.cpp
+++ test/similarity_test.cpp
@@ -8,3 +8,13 @@ TEST(SimilarityTest, CosineSimilarity) {
     double result = similarity::cosineSimilarity(v1, v2);
     ASSERT_NEAR(result, 0.9746318461970762, 1e-9);
 }
+
+TEST(NDCGTest, NDCG) {
+    std::vector<double> expected = {3.0, 2.0, 3.0, 0.0, 1.0, 2.0, 4.0, 5.0};
+    std::vector<double> answer = {3.0, 2.0, 3.0, 0.0, 1.0, 2.0, 6.0, 7.0};
+    double result = similarity::nDCG(expected, answer, 4);
+    ASSERT_NEAR(result, 1.0, 1e-9);
+
+    result = similarity::nDCG(expected, answer);
+    ASSERT_NEAR(result, 1.0, 1e-9);
+}
