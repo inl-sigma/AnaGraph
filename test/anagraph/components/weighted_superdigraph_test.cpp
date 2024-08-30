e9d8fd3570079745e368e916007e1bb5a2bf0309
inl-sigma <sigma@inl.ics.keio.ac.jp>
2024-08-30 23:40:45 +0900

    test: Add unit test to validate merge function in weighted directed supergraph

--- test/anagraph/components/weighted_superdigraph_test.cpp
+++ test/anagraph/components/weighted_superdigraph_test.cpp
@@ -281,7 +281,6 @@ TEST(WeightedSuperDigraphTest, GetAdjacents) {
 
 TEST(WeightedSuperDigraphTest, Size) {
     using namespace anagraph::graph_structure;
-    using namespace anagraph;
     spdlog::set_level(spdlog::level::debug);
     WeightedSuperDigraph graph;
     graph.setNode(1);
@@ -289,6 +288,76 @@ TEST(WeightedSuperDigraphTest, Size) {
     graph.setNode(3);
 
     EXPECT_EQ(graph.size(), static_cast<size_t>(3));
+}
+
+TEST(WeightedSuperDigraphTest, Merge) {
+    using namespace anagraph::graph_structure;
+    spdlog::set_level(spdlog::level::debug);
+    WeightedSuperDigraph graph;
+    graph.setNode(1);
+    graph.setNode(2);
+    graph.setNode(3);
+    graph.setNode(4);
+    graph.setNode(5);
+
+    graph.addEdge(1, 3, 1.0);
+    graph.addEdge(1, 5, 2.0);
+    graph.addEdge(2, 4, 1.5);
+    graph.addEdge(2, 5, 1.0);
+    graph.mergeNode(1, 2, [](WeightedSupernode first, WeightedSupernode second) {
+        WeightedSupernode node;
+        std::unordered_map<int, double> adjacents1 = first.getAdjacents();
+        std::unordered_map<int, double> adjacents2 = second.getAdjacents();
+        for (const auto &[id, weight] : adjacents1) {
+            node.setAdjacent(id, weight);
+            
+        }
+        for (const auto &[id, weight] : adjacents2) {
+            node.updateAdjacent(id, weight);
+        }
+        return node;
+    });
+
+    EXPECT_EQ(graph.size(), static_cast<size_t>(6));
+    EXPECT_EQ(graph.getWeight(6, 3), 1.0);
+    EXPECT_EQ(graph.getWeight(6, 4), 1.5);
+    EXPECT_EQ(graph.getWeight(6, 5), 3.0);
+}
+
+TEST(WeightedSuperDigraphTest, setMergeNodeFunction) {
+    using namespace anagraph::graph_structure;
+    WeightedSuperDigraph graph;
+    graph.setNode(1);
+    graph.setNode(2);
+    graph.setNode(3);
+    graph.setNode(4);
+    graph.setNode(5);
+
+    graph.addEdge(1, 3, 1.0);
+    graph.addEdge(1, 5, 2.0);
+    graph.addEdge(2, 4, 1.5);
+    graph.addEdge(2, 5, 1.0);
+
+    EXPECT_THROW(graph.mergeNode(1, 2), std::bad_function_call);
+
+    graph.setMergeNodeFunction([](WeightedSupernode first, WeightedSupernode second) {
+        WeightedSupernode node;
+        std::unordered_map<int, double> adjacents1 = first.getAdjacents();
+        std::unordered_map<int, double> adjacents2 = second.getAdjacents();
+        for (const auto &[id, weight] : adjacents1) {
+            node.setAdjacent(id, weight);
+            
+        }
+        for (const auto &[id, weight] : adjacents2) {
+            node.updateAdjacent(id, weight);
+        }
+        return node;
+    });
+    graph.mergeNode(1, 2);
+    EXPECT_EQ(graph.size(), static_cast<size_t>(6));
+    EXPECT_EQ(graph.getWeight(6, 3), 1.0);
+    EXPECT_EQ(graph.getWeight(6, 4), 1.5);
+    EXPECT_EQ(graph.getWeight(6, 5), 3.0);
 }
 
 TEST(WeightedSuperDigraphTest, ReadGraph) {
