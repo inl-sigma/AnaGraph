9a632188eb033858bbd4f515dd42cf078182f279
inl-sigma <sigma@inl.ics.keio.ac.jp>
2024-09-01 16:51:21 +0900

    test: Add test for getting and setting adjacent nodes

--- test/anagraph/components/weighted_supernode_test.cpp
+++ test/anagraph/components/weighted_supernode_test.cpp
@@ -5,13 +5,22 @@
 TEST(WeightedSupernodeTest, DefaultConstructor) {
     using namespace anagraph::graph_structure;
     WeightedSupernode node1;
-    ASSERT_EQ(node1.getId(), WeightedSupernode::UNUSED_ID);
-    ASSERT_FALSE(node1.isUsed());
+    ASSERT_EQ(node1.getId(), 0);
+    ASSERT_TRUE(node1.isUsed());
     ASSERT_TRUE(node1.isRoot());
     ASSERT_TRUE(node1.isLeaf());
     ASSERT_EQ(node1.getParent(), WeightedSupernode::ROOT);
     ASSERT_TRUE(node1.getChildren().empty());
     ASSERT_TRUE(node1.getAdjacents().empty());
+
+    WeightedSupernode node2;
+    ASSERT_EQ(node2.getId(), 1);
+    ASSERT_TRUE(node2.isUsed());
+    ASSERT_TRUE(node2.isRoot());
+    ASSERT_TRUE(node2.isLeaf());
+    ASSERT_EQ(node2.getParent(), WeightedSupernode::ROOT);
+    ASSERT_TRUE(node2.getChildren().empty());
+    ASSERT_TRUE(node2.getAdjacents().empty());
 }
 
 TEST(WeightedSupernodeTest, ConstructorWithId) {
@@ -59,6 +68,26 @@ TEST(WeightedSupernodeTest, SetAndGetAdjacent) {
     ASSERT_EQ(node1.getAdjacents().at(5), 1.5);
 }
 
+TEST(WeightedNodeTest, SetAndGetAdjacentNodes) {
+    using namespace anagraph;
+    graph_structure::WeightedSupernode node1(1);
+    graph_structure::WeightedSupernode node2(2);
+    graph_structure::WeightedSupernode node3(3);
+
+    node1.setAdjacentNode(node2, 1.5);
+    node1.setAdjacentNode(node3, 2.0);
+    const auto& adjacents = node1.getAdjacentNodes();
+    EXPECT_EQ(static_cast<int>(adjacents.size()), 2);
+    EXPECT_TRUE(adjacents.contains(2));
+    EXPECT_TRUE(adjacents.contains(3));
+
+    auto& adj = adjacents.at(2).get();
+    adj.setAdjacentNode(node3, 1.5);    
+    EXPECT_EQ(adj.getId(), 2);
+    EXPECT_TRUE(adj.getAdjacents().contains(3));
+    EXPECT_TRUE(node2.getAdjacents().contains(3));
+}
+
 TEST(WeightedSupernodeTest, UpdateAdjacent) {
     using namespace anagraph::graph_structure;
     WeightedSupernode node1;
@@ -77,6 +106,14 @@ TEST(WeightedSupernodeTest, RemoveAdjacent) {
     ASSERT_EQ(node1.getAdjacents().at(5), 1.5);
     node1.removeAdjacent(5);
     ASSERT_TRUE(node1.getAdjacents().empty());
+}
+
+TEST(WeightedSupernodeTest, GetWeight) {
+    using namespace anagraph::graph_structure;
+    WeightedSupernode node1;
+    node1.setAdjacent(5, 1.5);
+    ASSERT_EQ(node1.getWeight(5), 1.5);
+    ASSERT_EQ(node1.getWeight(6), 0.0);
 }
 
 TEST(WeightedSupernodeTest, Clear) {
